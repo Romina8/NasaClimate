8c18245181e75b0601bd76d96e9c9672d0410f82
Romina8 <guerrero17romina@gmail.com>
2020-08-03 13:17:35 -0300

    Se agregan los importes a los repos.

--- nasaclimate/src/main/java/ar/com/ada/api/nasaclimate/repositories/PaisRepository.java
+++ nasaclimate/src/main/java/ar/com/ada/api/nasaclimate/repositories/PaisRepository.java
@@ -1,6 +1,10 @@
 package ar.com.ada.api.nasaclimate.repositories;
 
-public interface PaisRepository extends JpaRepository <Pais, Interger>{
+import ar.com.ada.api.nasaclimate.entities.Pais;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface PaisRepository extends JpaRepository<Pais, Interger>{
 
 
 
--- nasaclimate/src/main/java/ar/com/ada/api/nasaclimate/repositories/TemperaturaRepository.java
+++ nasaclimate/src/main/java/ar/com/ada/api/nasaclimate/repositories/TemperaturaRepository.java
@@ -1,6 +1,11 @@
 package ar.com.ada.api.nasaclimate.repositories;
 
+
+import ar.com.ada.api.nasaclimate.entities.Temperatura;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
-public interface TemperaturaRepository extends JpaRepository <Temperatura, Interer>{
+public interface TemperaturaRepository extends JpaRepository<Temperatura, Interer>{
 
 }
